da46370c2e5c81e6e05535a806a92c6b98b4de73
tommy <tommy@bpo.accountants>
2025-01-09 12:10:43 +0300

    google analytic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from "react-dom"
+
+ReactGA.initialize("G-PRX3ECQJ26");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
